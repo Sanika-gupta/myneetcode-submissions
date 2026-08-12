fcc9ebaf1c55b1347bbff9911e75145fbce6b18d
Sanika <sanikagupto@gmail.com>
2026-08-12 22:48:38 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # iterate through nums for i check if i exists in nums
+        # if i exists in array move to next number in array 
+        res = []
+               
+        # for each number x in nums:
+        #     count how many times x appears in nums
+        #     if count == 1:
+        #         return x
+        for i in range(len(nums)):
+            count = 0
+            for j in range( len(nums)):
+                if(nums[i]==nums[j]):
+                    count+=1
+            if count == 1:
+                return nums[i]
+       
+
+
+        
