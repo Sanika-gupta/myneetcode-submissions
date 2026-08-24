3054fc858f1f2491787e514073c7607402c61334
Sanika <sanikagupto@gmail.com>
2026-08-24 18:39:55 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+      
+        # min bcs - thats max height water can stay w/o spilling
+        # trapped = min(heights[l],heights[r]) - heights[i]
+        if not height: return 0 
+        res=0
+        l = 0
+        r = len(height)-1
+        lMax = height[l]
+        rMax = height[r]
+        while l < r:
+            if lMax < rMax:
+                l+=1
+                lMax = max(lMax,height[l])
+                res += lMax - height[l]
+            else:
+                r-=1
+                rMax = max(rMax, height[r])
+                res += rMax - height[r]
+        return res
+
